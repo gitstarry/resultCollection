a80494abfe45bae9d9a7901f758c90d52f213e48
飘逸冷风 <992068309@qq.com>
2019-12-16 19:51:42 +0800

    改了一些数据 张鑫

--- src/components/common/NavMenu.vue
+++ src/components/common/NavMenu.vue
@@ -7,7 +7,7 @@
     text-color="#222"
     active-text-color="red"
     style="min-width: 1100px">
-    <span style="position: absolute;padding-top: 20px;right: 3%;font-size: 20px;font-weight: bold">学生录入后台管理中心</span>
+    <span style="position: absolute;padding-top: 20px;right: 3%;font-size: 20px;font-weight: bold">学生获奖录入中心</span>
     <el-menu-item v-for="(item,i) in navList" :key="i" :index="item.name">
       {{ item.navItem }}
     </el-menu-item>
@@ -21,8 +21,8 @@
             return {
                 navList: [
                     {name: '/index', navItem: '主页'},
-                    {name: '/jotter', navItem: '修改密码'},
-                    {name: '/library', navItem: '退出登录'},
+                    {name: '/student',navItem: '学生'},
+                    {name: '/information',navItem:'个人中心'},
                     {name: '/inputInfo', navItem: '录入比赛信息'}
                 ]
             }
